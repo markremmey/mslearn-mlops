580501e1d79a8deb33b8983a572fbca330b8e480
markremmey <53156818+markremmey@users.noreply.github.com>
2022-08-28 16:26:47 -0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -35,16 +35,23 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
-    return(X_train, X_test, y_train, y_test)
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 
+               'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 
+               'Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                        y, 
+                                                        test_size=0.30, 
+                                                        random_state=0)
+    return(X_train, X_test, y_train, y_test) 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
+    
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
 
 def parse_args():
+    
     # setup arg parser
     parser = argparse.ArgumentParser()
 
